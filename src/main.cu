cfa7fb510dc893f1600801126d7c6f1732e56115
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2012-10-31 15:14:17 +0530

    Synaptic connections manipulated on device
    Introduce infrastructure to manipulate synaptic connections on device. The code
    in its present form is only for show purposes. I haven't yet been able to
    isolate tests, because deadlines are too close.

--- src/main.cu
+++ src/main.cu
@@ -3,38 +3,59 @@
 #include "main.cuh"
 
 /* Step forward in time. */
-__global__ void time_step(int *dev_tim) 
+__global__ void time_step(int *dev_time)
 {
-    *dev_time += 1;
+  *dev_time += 1; 
 }
 
+__global__ void change_conn(Connection *dev_connections) 
+{
+  dev_connections[dev_get_index(1,12)].neuron = 14141;
+}
 
 int main( void ) 
 {
     int *dev_time;
     int host_time = 0;
-    time_t lt;
+
+    Connection host_connections[NUMNEURON * PSYNCONN]; 
+    Connection *dev_connections;
 
     // Allocate memory on the GPU
     cudaMalloc( (void**)&dev_time, sizeof(int) );
+    cudaMalloc( (void**)&dev_connections,
+        sizeof(Connection) * NUMNEURON * PSYNCONN);
 
     // Copy the time to the GPU
     cudaMemcpy(dev_time, &host_time, sizeof(int),
         cudaMemcpyHostToDevice);
 
+    // Copy the connections to the GPU
+    cudaMemcpy(dev_connections, &host_connections,
+        sizeof(Connection) * NUMNEURON * PSYNCONN,
+        cudaMemcpyHostToDevice);
+
     // XXX The limit shouldn't be iterations. Discuss!
     while (host_time < ITERATIONS) {
-        // Step forward in time.  Since this is a part of the global memory,
-        // you only need to do it via one thread.
+        // Step forward in time.  Since this is a part of the global
+        // memory, you only need to do it via one thread.
         time_step<<<1,1>>>(dev_time);
 
-        // Copy the time back to the CPU
+        // copy the time back to the cpu
         cudaMemcpy(&host_time, dev_time, sizeof(int),
-            cudaMemcpyDeviceToHost);
+            cudaMemcpyDeviceToHost); 
     }
+
+    change_conn<<<1,1>>>(dev_connections);
+
+    cudaMemcpy(&host_connections, dev_connections,
+        sizeof(Connection) * NUMNEURON * PSYNCONN,
+        cudaMemcpyDeviceToHost);
+
+    printf("%d\n", host_connections[get_index(1,12)].neuron);
 
     // Free memory on GPU
     cudaFree(dev_time);
 
-    return 0;
+    return 0; 
 }
--- src/main.cuh
+++ src/main.cuh
@@ -1,14 +1,73 @@
 #ifndef MOMENT_MAIN
 
-#define NEURONS_PER_LAYER  10000
-#define LAYERS  6
+/* A note about definitions as used in this program.
+  
+  LAYCOUNT  : Layer Count
+  NEUPERLAY : Neurons per layer.
+  OUTNEURON : Number of output neurons
+  INNEURON  : Number of input neurons
+  PSYNCONN  : Number of post-synaptic connections per neuron
 
-// Post synaptic connections per neuron: should be clear enough though
-#define POST_SYN_CONN_PER_NEURON  50
+  The Network.
+  ---
 
-// At this time, we are limiting the execution of the network to a finite
-// number of time steps.  In real applications, this is probably sub-optimal.
-// We need to figure out a better limiting condition.
+  There are INNEURON number of input neurons.  Each of these neurons
+  are connected to arbitrarily chosen PSYNCONN number of neurons in
+  the first layer of the LAYCOUNT nubmer of layers in the network.
+
+  Each neuron in the first layer is connected to arbitrary PSYNCONN
+  number of neurons in the second layer and so on till the final
+  layer.
+
+  Each neuron in the last number fans in to one of the final output
+  neurons.  This is ordered.  The first NEUPERLAY / OUTNEURON connect
+  to the first output neuron, the next NEUPERLAY / OUTNEURON connect
+  to the second and so on.  Any rounding off errors go to the final
+  neuron.
+
+
+  Reason for the tiny names
+  ---
+
+  Although it was possible to give truly mnemonic names to the
+  definitions, they would have been unwieldingly large and would have
+  cause legibility problems in the code.
+
+  Future me, I am sorry, but this needs to be done.  :(
+ */
+
+#define LAYCOUNT  6
+#define NEUPERLAY 10000
+#define OUTNEURON 12
+#define INNEURON  12
+#define PSYNCONN  50 
+
+// Computed directive counts neurons with outgoing connection
+#define NUMNEURON LAYCOUNT * NEUPERLAY + INNEURON
+
+// At this time, we are limiting the execution of the network to a
+// finite number of time steps.  In real applications, this is
+// probably sub-optimal.  We need to figure out a better limiting
+// condition.
 #define ITERATIONS 100000
+
+// Structure representing a connection. Has the id of the post
+// synaptic neuron and the axonal delay
+struct Connection {
+  int neuron;
+  int delay;
+};
+
+/* Given a neuron index and a connection index, convert to an offset
+ * useable on the 1D array that is connections.
+ */
+int get_index(int neuron, int connection) {
+  return neuron*PSYNCONN + connection;
+}
+
+/* GPU version of the same */
+__device__ int dev_get_index(int neuron, int connection) {
+  return neuron*PSYNCONN + connection;
+}
 
 #endif
