1e4b5eb9d78670194a3e57418b7ff4731e06121a
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2012-11-01 15:28:07 +0530

    Indentation inconsistency fixed

--- src/main.cu
+++ src/main.cu
@@ -19,55 +19,55 @@ __global__ void time_step(int *dev_time)
 /* The main moment loop */
 int main( void ) 
 {
-    int *dev_time;
-    int host_time = 0;
+  int *dev_time;
+  int host_time = 0;
 
-    Connection host_connections[NUMNEURON * PSYNCONN]; 
-    Connection *dev_connections;
+  Connection host_connections[NUMNEURON * PSYNCONN]; 
+  Connection *dev_connections;
 
-    Neuron host_neurons[NUMNEURON + OUTNEURON];
-    Neuron *dev_neurons;
+  Neuron host_neurons[NUMNEURON + OUTNEURON];
+  Neuron *dev_neurons;
 
-    // Allocate memory on the GPU
-    cudaMalloc( (void**)&dev_time, sizeof(int) );
-    cudaMalloc( (void**)&dev_connections,
-        sizeof(Connection) * NUMNEURON * PSYNCONN);
-    cudaMalloc( (void**)&dev_neurons,
-        sizeof(Neuron) * (NUMNEURON + OUTNEURON) );
+  // Allocate memory on the GPU
+  cudaMalloc( (void**)&dev_time, sizeof(int) );
+  cudaMalloc( (void**)&dev_connections,
+      sizeof(Connection) * NUMNEURON * PSYNCONN);
+  cudaMalloc( (void**)&dev_neurons,
+      sizeof(Neuron) * (NUMNEURON + OUTNEURON) );
 
-    // Initialization
-    srand( time(NULL) ); // Random seed
-    input_random_current(host_neurons);
+  // Initialization
+  srand( time(NULL) ); // Random seed
+  input_random_current(host_neurons);
 
-    for (int i=0; i<INNEURON; i++) {
-      printf("%d\n", host_neurons[i].current);
-    }
+  for (int i=0; i<INNEURON; i++) {
+    printf("%d\n", host_neurons[i].current);
+  }
 
-    // Copy the time to the GPU
-    cudaMemcpy(dev_time, &host_time, sizeof(int),
-        cudaMemcpyHostToDevice);
+  // Copy the time to the GPU
+  cudaMemcpy(dev_time, &host_time, sizeof(int),
+      cudaMemcpyHostToDevice);
 
-    // Copy the connections to the GPU
-    cudaMemcpy(dev_connections, &host_connections,
-        sizeof(Connection) * NUMNEURON * PSYNCONN,
-        cudaMemcpyHostToDevice);
+  // Copy the connections to the GPU
+  cudaMemcpy(dev_connections, &host_connections,
+      sizeof(Connection) * NUMNEURON * PSYNCONN,
+      cudaMemcpyHostToDevice);
 
-    while (host_time < ITERATIONS) {
-        // Step forward in time.  Since this is a part of the global
-        // memory, you only need to do it via one thread.
-        time_step<<<1,1>>>(dev_time);
+  while (host_time < ITERATIONS) {
+      // Step forward in time.  Since this is a part of the global
+      // memory, you only need to do it via one thread.
+      time_step<<<1,1>>>(dev_time);
 
-        // copy the time back to the cpu
-        cudaMemcpy(&host_time, dev_time, sizeof(int),
-            cudaMemcpyDeviceToHost); 
-    }
+      // copy the time back to the cpu
+      cudaMemcpy(&host_time, dev_time, sizeof(int),
+          cudaMemcpyDeviceToHost); 
+  }
 
-    cudaMemcpy(&host_connections, dev_connections,
-        sizeof(Connection) * NUMNEURON * PSYNCONN,
-        cudaMemcpyDeviceToHost);
+  cudaMemcpy(&host_connections, dev_connections,
+      sizeof(Connection) * NUMNEURON * PSYNCONN,
+      cudaMemcpyDeviceToHost);
 
-    // Free memory on GPU
-    cudaFree(dev_time);
+  // Free memory on GPU
+  cudaFree(dev_time);
 
-    return 0; 
+  return 0; 
 }
