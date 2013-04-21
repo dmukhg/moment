202eedc928086380d95a7f23a05d8b15004f3c1f
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2013-04-22 02:35:43 +0530

    Do sampling run for vib-analysis

--- src/vibration-analysis.cu
+++ src/vibration-analysis.cu
@@ -33,6 +33,10 @@ void test_vibration_analysis(Network *n) {
         feed[S_C],
         VB[S_C];
 
+  Neuron *neu;
+  Connection *con;
+  int *rate;
+
   // Store data
   for (i=0; i<S_C; i++) {
     sprintf(filelocation, "%s/processed/vib_table-%d-energies.data", DIRECTORY, i);
@@ -132,6 +136,61 @@ void test_vibration_analysis(Network *n) {
     fp = openfile(filelocation);
     fscanf(fp, "%f", &ta);
     VB[i] = ta;
+  }
+  // Memorized all data
+
+  // Perform initial sampling to figure out cluster centres
+  for (i=0; i<S_C; i++) {
+    // Reset inputs 
+    neu = n->neurons(true);
+    for (int xii=0; xii<22; xii++) {
+      neu[xii].input = 0;
+    }
+
+    n->neurons(neu, true);
+
+    for (int j=0; j<1000; j++) {
+      if (j == 100) { // Allow time for network to stabilize
+        neu = n->neurons(true);
+        neu[0].input = vib_table[i][0];
+        neu[1].input = vib_table[i][1];
+        neu[2].input = vib_table[i][2];
+        neu[3].input = vib_table[i][3];
+        neu[4].input = vib_spindle[i][0];
+        neu[5].input = vib_spindle[i][1];
+        neu[6].input = vib_spindle[i][2];
+        neu[7].input = vib_spindle[i][3];
+        neu[8].input = AE_table[i][0];
+        neu[9].input = AE_table[i][1];
+        neu[10].input = AE_table[i][2];
+        neu[11].input = AE_table[i][3];
+        neu[12].input = AE_spindle[i][0];
+        neu[13].input = AE_spindle[i][1];
+        neu[14].input = AE_spindle[i][2];
+        neu[15].input = AE_spindle[i][3];
+        neu[16].input = smcDC[i][0];
+        neu[17].input = smcDC[i][1];
+        neu[18].input = smcDC[i][2];
+        neu[19].input = smcDC[i][3];
+        neu[20].input = feed[i];
+        neu[21].input = DOC[i];
+
+        for (int xii=0; xii<22; xii++) {
+          //neu[xii].input = 100;
+        }
+
+        n->neurons(neu, true);
+      }
+
+      n->time_step();
+      n->find_firing_neurons();
+      n->update_current();
+      n->update_potential();
+    }
+
+    rate = n->spiking_rate(true);
+
+    printf("%d\n", rate[4]);
   }
 }
 
