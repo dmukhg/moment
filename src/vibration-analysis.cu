c2c0d8ac57a7a54682d18a378e257d242def8b0b
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2013-04-24 01:35:52 +0530

    Normalize values before feeding to network

--- src/vibration-analysis.cu
+++ src/vibration-analysis.cu
@@ -5,7 +5,7 @@
 
 #include "network.cuh"
 
-#define S_C 167 // Sample Count
+#define S_C 16 // Sample Count
 #define TS_F 4  // Data point per time-series
 #define DIRECTORY "data/"
 
@@ -33,9 +33,18 @@ void test_vibration_analysis(Network *n) {
         feed[S_C],
         VB[S_C];
 
+  float peak_vib_table[4] = { 0.0, 0.0, 0.0, 0.0},
+        peak_vib_spindle[4] = { 0.0, 0.0, 0.0, 0.0},
+        peak_AE_table[4] = { 0.0, 0.0, 0.0, 0.0},
+        peak_AE_spindle[4] = { 0.0, 0.0, 0.0, 0.0},
+        peak_smcDC[4] = { 0.0, 0.0, 0.0, 0.0},
+        peak_DOC = 0.0,
+        peak_feed = 0.0,
+        peak_VB = 0.0;
+
   Neuron *neu;
   Connection *con;
-  int *rate;
+  int *rate, normalization_scaling_factor = 20;
 
   // Store data
   for (i=0; i<S_C; i++) {
@@ -48,10 +57,14 @@ void test_vibration_analysis(Network *n) {
     fscanf(fp, "%f, %f\n", &ta, &tb); 
     vib_table[i][0] = ta;
     vib_table[i][1] = tb;
+    peak_vib_table[0] = ta > peak_vib_table[0] ? ta : peak_vib_table[0];
+    peak_vib_table[1] = tb > peak_vib_table[1] ? tb : peak_vib_table[1];
 
     fscanf(fp, "%f, %f\n", &ta, &tb); 
     vib_table[i][2] = ta;
     vib_table[i][3] = tb;
+    peak_vib_table[2] = ta > peak_vib_table[2] ? ta : peak_vib_table[2];
+    peak_vib_table[3] = tb > peak_vib_table[3] ? tb : peak_vib_table[3];
 
     fclose(fp);
 
@@ -64,10 +77,14 @@ void test_vibration_analysis(Network *n) {
     fscanf(fp, "%f, %f\n", &ta, &tb); 
     vib_spindle[i][0] = ta;
     vib_spindle[i][1] = tb;
+    peak_vib_spindle[0] = ta > peak_vib_spindle[0] ? ta : peak_vib_spindle[0];
+    peak_vib_spindle[1] = tb > peak_vib_spindle[1] ? tb : peak_vib_spindle[1];
 
     fscanf(fp, "%f, %f\n", &ta, &tb); 
     vib_spindle[i][2] = ta;
     vib_spindle[i][3] = tb;
+    peak_vib_spindle[2] = ta > peak_vib_spindle[2] ? ta : peak_vib_spindle[2];
+    peak_vib_spindle[3] = tb > peak_vib_spindle[3] ? tb : peak_vib_spindle[3];
 
     fclose(fp);
 
@@ -80,10 +97,14 @@ void test_vibration_analysis(Network *n) {
     fscanf(fp, "%f, %f\n", &ta, &tb); 
     AE_table[i][0] = ta;
     AE_table[i][1] = tb;
+    peak_AE_table[0] = ta > peak_AE_table[0] ? ta : peak_AE_table[0];
+    peak_AE_table[1] = tb > peak_AE_table[1] ? tb : peak_AE_table[1];
 
     fscanf(fp, "%f, %f\n", &ta, &tb); 
     AE_table[i][2] = ta;
     AE_table[i][3] = tb;
+    peak_AE_table[2] = ta > peak_AE_table[2] ? ta : peak_AE_table[2];
+    peak_AE_table[3] = tb > peak_AE_table[3] ? tb : peak_AE_table[3];
 
     fclose(fp);
 
@@ -96,10 +117,14 @@ void test_vibration_analysis(Network *n) {
     fscanf(fp, "%f, %f\n", &ta, &tb); 
     AE_spindle[i][0] = ta;
     AE_spindle[i][1] = tb;
+    peak_AE_spindle[0] = ta > peak_AE_spindle[0] ? ta : peak_AE_spindle[0];
+    peak_AE_spindle[1] = tb > peak_AE_spindle[1] ? tb : peak_AE_spindle[1];
 
     fscanf(fp, "%f, %f\n", &ta, &tb); 
     AE_spindle[i][2] = ta;
     AE_spindle[i][3] = tb;
+    peak_AE_spindle[2] = ta > peak_AE_spindle[2] ? ta : peak_AE_spindle[2];
+    peak_AE_spindle[3] = tb > peak_AE_spindle[3] ? tb : peak_AE_spindle[3];
 
     fclose(fp);
 
@@ -112,10 +137,14 @@ void test_vibration_analysis(Network *n) {
     fscanf(fp, "%f, %f\n", &ta, &tb); 
     smcDC[i][0] = ta;
     smcDC[i][1] = tb;
+    peak_smcDC[0] = ta > peak_smcDC[0] ? ta : peak_smcDC[0];
+    peak_smcDC[1] = tb > peak_smcDC[1] ? tb : peak_smcDC[1];
 
     fscanf(fp, "%f, %f\n", &ta, &tb); 
     smcDC[i][2] = ta;
     smcDC[i][3] = tb;
+    peak_smcDC[2] = ta > peak_smcDC[2] ? ta : peak_smcDC[2];
+    peak_smcDC[3] = tb > peak_smcDC[3] ? tb : peak_smcDC[3];
 
     fclose(fp);
 
@@ -124,20 +153,36 @@ void test_vibration_analysis(Network *n) {
     fp = openfile(filelocation);
     fscanf(fp, "%f", &ta);
     feed[i] = ta;
+    peak_feed = ta > peak_feed ? ta : peak_feed;
 
      // Read DOC
     sprintf(filelocation, "%s/raw/DOC-%d.data", DIRECTORY, i);
     fp = openfile(filelocation);
     fscanf(fp, "%f", &ta);
     DOC[i] = ta;
+    peak_DOC = ta > peak_DOC ? ta : peak_DOC;
 
      // Read FB
     sprintf(filelocation, "%s/raw/VB-%d.data", DIRECTORY, i);
     fp = openfile(filelocation);
     fscanf(fp, "%f", &ta);
     VB[i] = ta;
+    peak_VB = ta > peak_VB ? ta : peak_VB;
   }
   // Memorized all data
+
+  // Divide all peak values by a normalization scaling factor
+  for (int i=0; i<4; i++) {
+    peak_vib_table[i] /= normalization_scaling_factor;
+    peak_vib_spindle[i] /= normalization_scaling_factor;
+    peak_AE_table[i] /= normalization_scaling_factor;
+    peak_AE_spindle[i] /= normalization_scaling_factor;
+    peak_smcDC[i] /= normalization_scaling_factor;
+  }
+
+  peak_feed /= normalization_scaling_factor;
+  peak_DOC /= normalization_scaling_factor;
+  peak_VB /= normalization_scaling_factor;
 
   // Perform initial sampling to figure out cluster centres
   for (i=0; i<S_C; i++) {
@@ -152,32 +197,28 @@ void test_vibration_analysis(Network *n) {
     for (int j=0; j<1000; j++) {
       if (j == 100) { // Allow time for network to stabilize
         neu = n->neurons(true);
-        neu[0].input = vib_table[i][0];
-        neu[1].input = vib_table[i][1];
-        neu[2].input = vib_table[i][2];
-        neu[3].input = vib_table[i][3];
-        neu[4].input = vib_spindle[i][0];
-        neu[5].input = vib_spindle[i][1];
-        neu[6].input = vib_spindle[i][2];
-        neu[7].input = vib_spindle[i][3];
-        neu[8].input = AE_table[i][0];
-        neu[9].input = AE_table[i][1];
-        neu[10].input = AE_table[i][2];
-        neu[11].input = AE_table[i][3];
-        neu[12].input = AE_spindle[i][0];
-        neu[13].input = AE_spindle[i][1];
-        neu[14].input = AE_spindle[i][2];
-        neu[15].input = AE_spindle[i][3];
-        neu[16].input = smcDC[i][0];
-        neu[17].input = smcDC[i][1];
-        neu[18].input = smcDC[i][2];
-        neu[19].input = smcDC[i][3];
-        neu[20].input = feed[i];
-        neu[21].input = DOC[i];
-
-        for (int xii=0; xii<22; xii++) {
-          //neu[xii].input = 100;
-        }
+        neu[0].input = vib_table[i][0] / peak_vib_table[0];
+        neu[1].input = vib_table[i][1] / peak_vib_table[1];
+        neu[2].input = vib_table[i][2] / peak_vib_table[2];
+        neu[3].input = vib_table[i][3] / peak_vib_table[3];
+        neu[4].input = vib_spindle[i][0] / peak_vib_spindle[0];
+        neu[5].input = vib_spindle[i][1] / peak_vib_spindle[1];
+        neu[6].input = vib_spindle[i][2] / peak_vib_spindle[2];
+        neu[7].input = vib_spindle[i][3] / peak_vib_spindle[3];
+        neu[8].input = AE_table[i][0] / peak_AE_table[0];
+        neu[9].input = AE_table[i][1] / peak_AE_table[1];
+        neu[10].input = AE_table[i][2] / peak_AE_table[2];
+        neu[11].input = AE_table[i][3] / peak_AE_table[3];
+        neu[12].input = AE_spindle[i][0] / peak_AE_spindle[0];
+        neu[13].input = AE_spindle[i][1] / peak_AE_spindle[1];
+        neu[14].input = AE_spindle[i][2] / peak_AE_spindle[2];
+        neu[15].input = AE_spindle[i][3] / peak_AE_spindle[3];
+        neu[16].input = smcDC[i][0] / peak_smcDC[0];
+        neu[17].input = smcDC[i][1] / peak_smcDC[1];
+        neu[18].input = smcDC[i][2] / peak_smcDC[2];
+        neu[19].input = smcDC[i][3] / peak_smcDC[3];
+        neu[20].input = feed[i] / peak_feed;
+        neu[21].input = DOC[i] / peak_DOC;
 
         n->neurons(neu, true);
       }
@@ -185,19 +226,26 @@ void test_vibration_analysis(Network *n) {
       n->time_step();
       n->find_firing_neurons();
       n->update_current();
+
+      /*neu = n->neurons(true);
+      printf("%d\n", neu[22].current);*/
+
+      /*con = n->connections(true);
+      printf("%d\n", con[21].neuron);*/
+
       n->update_potential();
     }
 
     rate = n->spiking_rate(true);
 
-    printf("%d\n", rate[4]);
+    printf("%d\n", rate[44]);
   }
 }
 
 int main() {
   Network *n = new Network(45, 506);
-  n->build_connections(22,22,1);
   n->randomize_weights();
+  n->build_connections(22,22,1);
 
   test_vibration_analysis(n);
 }
